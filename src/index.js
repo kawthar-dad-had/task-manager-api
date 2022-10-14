af9f0c509c17adc978eb71211b67b1701d218ce9
kawthar-dad-had <kawdad24@gmail.com>
2022-10-14 15:06:04 +0100

    add

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const taskRouter = require('./routers/task')
 
 
 const app = express()
-const port = process.env.PORT
+const port = process.env.PORT||3000
 
 app.use((req,res,next) => {
     console.log(req.method ,req.path)
